9abc591085b4b24933de5b8110c77304ec5c8c0e
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-20 06:52:28 +0000

    [Finish] 4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,7 +15,6 @@ class BlogsController < ApplicationController
 	blog = Blog.new(blog_params)
 	blog.save
 	redirect_to blogs_path(blog.id)
-
   end
 
   def edit
@@ -26,6 +25,12 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
 
