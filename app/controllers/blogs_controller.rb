e379075f553e867e992a28ff4483b872e03eb371
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-20 00:11:28 +0000

    [Finish] 4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,12 +2,27 @@ class BlogsController < ApplicationController
   def index
   end
 
-  def sho
+  def show
   end
 
   def new
+  	@blog = Blog.new
+  end
+
+  def create
+	blog = Blog.new(blog_params)
+	blog.save
+	redirect_to blogs_path
+
   end
 
   def edit
   end
+
+
+
+  	private
+  	def blog_params
+  		params.require(:blog).permit(:title, :category, :body)
+  	end
 end
